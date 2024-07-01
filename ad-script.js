e4d5c9630e77a834f21278cefe75f896764b649a
Grid7 <73asdzzz@gmail.com>
2024-07-02 02:42:30 +0900

    “coupangad”

--- ad-script.js
+++
@@ -1,2 +0,0 @@
-<script src="https://ads-partners.coupang.com/g.js"></script>
-new PartnersCoupang.G({"id":788196,"template":"carousel","trackingCode":"AF8698726","width":"680","height":"140","tsource":""});
