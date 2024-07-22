37f9e3088644ae0ba25bc446810d19e5bd03259a
Grid7 <73asdzzz@gmail.com>
2024-07-23 01:41:53 +0900

    “setting”

--- popup.js
+++ popup.js
@@ -53,7 +53,7 @@ const appData = [
                     <span style="font-size: 14px;">인공지능 모델, 이미지를 Live Portrait하여 다양한 곳에 사용.</span> 
                     <br><br>
                     <div style="text-align: center;">
-                        <img src="imgp/p5.png" alt="Feature Image" width="300" height="200">
+                        <img src="imgp/p3.png" alt="Feature Image" width="300" height="200">
                     </div>
                     <br><br>
                     <span style="font-size: 12px;">저작권 걱정없이 다운로드하여 영상의 품질을 극대화 시킬 수 있습니다.<span>`,
@@ -61,46 +61,6 @@ const appData = [
     },
     {   
         id: 4,
-        title: "Motion array",
-        image: "img/4.png",
-        features: ["100개 이상의 free video templates", "100개 이상의 free songs & SFX", "하루 10개 무료 다운로드 템플릿 제공"],
-        description: `무료 템플릿도 쓸만한게 많습니다. 처음 영상 공부하시는 분들께 추천. 
-                    <br><br> 
-                    <span style="font-size: 14px;">모션 어레이에서 무료 템플릿 받아보아요.</span> 
-                    <br><br>
-                    <span style="font-size: 14px;">품질이 좋은 영상, 이미지등 수많은 것들을 하루 10개까지 무료 다운로드하여 사용할 수 있는 곳입니다.</span> 
-                    <br><br>
-                    <div style="text-align: center;">
-                        <img src="imgp/p4.png" alt="Feature Image" width="300" height="200">
-                        <br><br>
-                        <img src="imgp/p4-1.png" alt="Feature Image" width="300" height="200">
-                    </div>
-                    <br><br>
-                    <span style="font-size: 12px;">저작권 걱정없는 무료 다운로드하여 영상의 품질을 극대화 시킬 수 있습니다.<span>`,
-        downloadLink: "https://motionarray.com",
-    },
-    {   
-        id: 5,
-        title: "Live Portrait",
-        image: "img/5.png",
-        features: ["100개 이상의 free video templates", "100개 이상의 free songs & SFX", "하루 10개 무료 다운로드 템플릿 제공"],
-        description: `무료 템플릿도 쓸만한게 많습니다. 처음 영상 공부하시는 분들께 추천. 
-                    <br><br> 
-                    <span style="font-size: 14px;">모션 어레이에서 무료 템플릿 받아보아요.</span> 
-                    <br><br>
-                    <span style="font-size: 14px;">품질이 좋은 영상, 이미지등 수많은 것들을 하루 10개까지 무료 다운로드하여 사용할 수 있는 곳입니다.</span> 
-                    <br><br>
-                    <div style="text-align: center;">
-                        <img src="imgp/p5.png" alt="Feature Image" width="300" height="200">
-                        <br><br>
-                        <img src="imgp/p4.png" alt="Feature Image" width="300" height="200">
-                    </div>
-                    <br><br>
-                    <span style="font-size: 12px;">저작권 걱정없는 무료 다운로드하여 영상의 품질을 극대화 시킬 수 있습니다.<span>`,
-        downloadLink: "https://huggingface.co/spaces/KwaiVGI/LivePortrait",
-    },
-    {   
-        id: 6,
         title: "pixabay",
         image: "img/3.png",
         features: ["무료 저작권 다운로드", "동영상, 이미지, 벡터, gif등 다양", "음악, 음향효과 다운로드"],
@@ -111,11 +71,51 @@ const appData = [
                     <span style="font-size: 14px;">품질이 좋은 영상, 이미지등 수많은 것들을 저작권 걱정없이 다운로드하여 사용할 수 있는 곳입니다.</span> 
                     <br><br>
                     <div style="text-align: center;">
-                        <img src="imgp/p3.png" alt="Feature Image" width="300" height="200">
+                        <img src="imgp/p4.png" alt="Feature Image" width="300" height="200">
                     </div>
                     <br><br>
                     <span style="font-size: 12px;">저작권 걱정없이 다운로드하여 영상의 품질을 극대화 시킬 수 있습니다.<span>`,
         downloadLink: "https://www.remove.bg",
+    },
+    {   
+        id: 5,
+        title: "Forensic Wortermark",
+        image: "img/4.jpg",
+        features: ["이미지 포렌식 워터마크", "영상 포렌식 워터마크", "포렌식 워터마크 생성"],
+        description: `SNS 인플루언서에게 가장 필수적인 웹 어플리케이션. 
+                    <br><br> 
+                    <span style="font-size: 14px;">Mark AI에서 무료 생성 포렌식 워터마크를 경험하세요.</span> 
+                    <br><br>
+                    <span style="font-size: 14px;">제한 없는 이미지 영상의 포렌식 워터마크를 통해 고퀄리티 영상의 저작권을 보호하세요.</span> 
+                    <br><br>
+                    <div style="text-align: center;">
+                        <img src="imgp/p5.png" alt="Feature Image" width="300" height="200">
+                        <br><br>
+                        <img src="imgp/p5-1.png" alt="Feature Image" width="300" height="200">
+                    </div>
+                    <br><br>
+                    <span style="font-size: 12px;">저작권 걱정없는 무료 다운로드하여 영상의 품질을 극대화 시킬 수 있습니다.<span>`,
+        downloadLink: "https://huggingface.co/spaces/KwaiVGI/LivePortrait",
+    },
+    {   
+        id: 6,
+        title: "Motion array",
+        image: "img/4.png",
+        features: ["100개 이상의 free video templates", "100개 이상의 free songs & SFX", "하루 10개 무료 다운로드 템플릿 제공"],
+        description: `무료 템플릿도 쓸만한게 많습니다. 처음 영상 공부하시는 분들께 추천. 
+                    <br><br> 
+                    <span style="font-size: 14px;">모션 어레이에서 무료 템플릿 받아보아요.</span> 
+                    <br><br>
+                    <span style="font-size: 14px;">품질이 좋은 영상, 이미지등 수많은 것들을 하루 10개까지 무료 다운로드하여 사용할 수 있는 곳입니다.</span> 
+                    <br><br>
+                    <div style="text-align: center;">
+                        <img src="imgp/p6.png" alt="Feature Image" width="300" height="200">
+                        <br><br>
+                        <img src="imgp/p6-1.png" alt="Feature Image" width="300" height="200">
+                    </div>
+                    <br><br>
+                    <span style="font-size: 12px;">저작권 걱정없는 무료 다운로드하여 영상의 품질을 극대화 시킬 수 있습니다.<span>`,
+        downloadLink: "https://motionarray.com",
     },
     {
         id: 30,
